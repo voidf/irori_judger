3fc5332a9c4e809621ec709f5c83e555d42ac258
VFC <12@3.4>
2020-10-07 01:50:49 +0800

    再见多文件

--- app/config/development.py
+++
@@ -1,8 +0,0 @@
-class DevelopmentConfig():
-    DEBUG = True
-    HOST = '127.0.0.1'
-    PORT = 19198
-    MONGODB_SETTINGS = {
-        'db': 'irori_OpenJudge',
-        'host': 'mongodb://database:27017/irori_OpenJudge',
-    }
