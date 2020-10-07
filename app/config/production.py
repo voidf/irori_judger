4d0833bf728af49fed70ef0b0e6694097e249484
Perseus <perseus@perseus.perseus>
2020-10-07 12:23:53 +0800

    abaaba

--- app/config/production.py
+++
@@ -1,8 +0,0 @@
-class ProductionConfig():
-    DEBUG = True
-    HOST = '127.0.0.1'
-    PORT = 19198
-    MONGODB_SETTINGS = {
-        'db': 'irori_OpenJudge',
-        'host': 'mongodb://database/irori_OpenJudge',
-    }
